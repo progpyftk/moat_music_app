40e84912e820f33f4a7f7b1c99fd6edd25a6d358
Lorenzo <progpyftk@yahoo.com>
2022-02-13 21:50:04 -0300

    user admin working fine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_123440) do
+ActiveRecord::Schema.define(version: 2022_02_14_001821) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_02_13_123440) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
